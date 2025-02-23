5773e216c7a0e0a975e9991af5443eef7dd7bd5f
vishnuguntika <guntikavishnu@gmail.com>
2025-02-22 20:10:54 -0600

    Laying the foundation

--- App.js
+++ App.js
@@ -1,65 +1,23 @@
-/**
- * HMR - Hot Module Replacement
- * File Watcher Algorithm -  Written in C++
- * BUNDLING
- * MINIFY
- * Cleaning our code
- * Dev and Production Build
- * Super fast build algorithm
- * Image Optimization
- * Caching while Development
- * Compression
- * Compatable with older versions of browser
- * HTTPS on dev
- * port Number
- * Consistent with Hashing Algorithm
- * Zero Config
- * 
- */
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-  "h1",
-  {
-    id: "title",
-  },
 
-  "Namaste Everyone!"
+const Title = () => (
+  <h1 id="title" key="h2">
+    Namaste React Guru
+  </h1>
 );
 
-const heading1 = React.createElement(
-  "h1",
-  {
-    id: "title",
-  },
-  "heading 1"
-);
-
-const heading2 = React.createElement(
-  "h2",
-  {
-    id: "title",
-  },
-  "heading 2"
-);
-
-const container = React.createElement(
-  "div",
-  {
-    id: "container",
-  },
-
-  [heading, heading1, heading2]
-);
-
-console.log(heading);
+// component composition.
+const HeaderComponent = () => {
+  return (
+    <div>
+      <Title/>
+      <h2>Namaste React Functional Component</h2>
+      <h2>This is a h2</h2>
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-//passing a react element inside the root
-// render - it takes the react element, and modifies the react dom
-// root.render(heading);
-
-// Learn about async and defer
-root.render(container);
+root.render(<HeaderComponent />);
